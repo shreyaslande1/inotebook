766f2753d3df535e9d9a741b7793e6120c63d25f
shreyaslande <shreyaslande200@gmail.com>
2026-06-28 18:23:34 +0530

    completed till the salt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "thisismysecretkey";
 
 // Create a user using POST "/api/auth/createuser"
+// Doesn't require authentication
 router.post(
     '/createuser',
     [
@@ -14,7 +19,7 @@ router.post(
 
     async (req, res) => {
 
-        // Check validation errors
+        // Check for validation errors
         const errors = validationResult(req);
 
         if (!errors.isEmpty()) {
@@ -22,26 +27,45 @@ router.post(
         }
 
         try {
-            //checking existinguser
-             let user = await User.findOne({ email: req.body.email });
-                console.log(user)
+
+            // Check whether the user already exists
+            let user = await User.findOne({ email: req.body.email });
+
             if (user) {
                 return res.status(400).json({
                     error: "Sorry, a user with this email already exists"
                 });
             }
-            // Create and save user
+
+            // Generate Salt
+            const salt = await bcrypt.genSalt(10);
+
+            // Secure Password
+            const secPass = await bcrypt.hash(req.body.password, salt);
+
+            // Create User
             user = await User.create({
                 name: req.body.name,
                 email: req.body.email,
-                password: req.body.password
+                password: secPass
             });
 
-            res.json(user);
+            // Data to be stored in JWT
+            const data = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            // Generate JWT Token
+            const authToken = jwt.sign(data, JWT_SECRET);
+
+            // Send token to user
+            res.json({ authToken });
 
         } catch (error) {
-           console.error(error.message);
-           res.status(500).send("Dont duplicate the values bro");
+            console.error(error.message);
+            res.status(500).send("Internal Server Error");
         }
     }
 );
