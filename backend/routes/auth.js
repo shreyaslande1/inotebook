1e86aebd1cbb1cc35daf2258131ee71a0ae466fe
shreyaslande <shreyaslande200@gmail.com>
2026-07-02 20:57:32 +0530

    env changes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,7 +5,7 @@ const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const fetchUser = require("../middleware/fetchUser")
-const JWT_SECRET = "thisismysecretkey";
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // Create a user using POST "/api/auth/createuser"
 // Doesn't require authentication
